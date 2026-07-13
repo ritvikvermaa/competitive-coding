77a8c5e114283db6a953b6d0803b33d9b9c1f7e2
Ritvik Verma <vritvik8@gmail.com>
2026-07-13 09:40:40 +0530

    Time: 23 ms (79.00%) | Memory: 75.9 MB (90.18%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer>window = new HashSet<>();
+        for (int i = 0; i < nums.length; i++){
+            if (i > k){
+                window.remove(nums[i-k-1]);
+            }
+            if (window.contains(nums[i])){
+                return true;
+            }
+            window.add(nums[i]);
+        }
+        return false;
+    }
+}
